05cf10257f3b01c2aaa552e53a128334fb79bcfe
Alexey Trilis <aatrilis2000@gmail.com>
2020-09-29 21:13:38 +0300

    futamura-2

--- hw03.rkt
+++ hw03.rkt
@@ -14,32 +14,31 @@
         tape
         (cdr tape))))
 
+(define (subst env expr)
+      (match expr
+        [`(,x . ,y) `(,(subst env x) . ,(subst env y))]
+        [`,x (if (hash-has-key? env x) (hash-ref env x) x)]))
+
 (define eval-expr
   (lambda (expr env)
-    (if (and (list? expr) (equal? (car expr) 'quote))
-        (cadr expr)
-        (if (list? expr)
-            (apply (eval (car expr) ns) (map (lambda (e) (eval-expr e env)) (cdr expr)))
-            (if (symbol? expr)
-                (hash-ref env expr)
-                expr))
-        )))
+    (eval (subst env expr) ns)))
 
 (define reduce
   (lambda (expr vs)
-    (if (and (list? expr) (equal? (car expr) 'quote))
-        expr
-        (if (list? expr)
-            (cons (car expr) (map (lambda (e) (reduce e vs)) (cdr expr)))
-            (hash-ref vs expr expr))
-        )))
+    (subst vs expr)))
 
 (define lookup
   (lambda (label program)
-    (if (equal? label (caar program))
-        (cdar program)
-        (lookup label (cdr program)))
-    ))
+    (dict-ref program label)))
+
+(define next-label-after
+  (lambda (label program)
+    (if (empty? (cdr program))
+        (caar program)
+        (if (equal? label (caar program))
+            (caadr program)
+            (next-label-after label (cdr program)))
+    )))
 
 (define is-static
   (lambda (expr division)
@@ -61,6 +60,22 @@
   (lambda (program division)
     (filter (lambda (k) (not (is-static k division))) (cdar program))))
 
+(define create-label-map
+  (lambda (program)
+     (make-immutable-hash (map cons (map car program) program))
+    )
+  )
+
+(define update-env
+  (lambda (env x v)
+    (hash-set env x (cons 'quote (list v)))))
+
+(define create-env-map
+  (lambda (program data)
+    (foldl (lambda (t env) (update-env env (car t) (cdr t))) #hash() (map cons (cdar program) data))
+    )
+  )
+
 (define improve-labels
   (lambda (program)
   (define change-labels
@@ -80,14 +95,14 @@
         (define run-command
           (lambda (instr new_env)
             (match instr
-              [`(:= ,v ,expr) (hash-set new_env v (eval-expr expr new_env))]
+              [`(:= ,v ,expr) (update-env new_env v (eval-expr expr new_env))]
               [`(return ,expr) (eval-expr expr new_env)]
               [`(goto ,label) (run-block (hash-ref labels label) labels new_env)]
               [`(if ,cond ,tbranch ,fbranch) (run-block (hash-ref labels (if (eval-expr cond new_env) tbranch fbranch)) labels new_env)]
             )))
         (foldl run-command env (cdr block))
         ))
-    (run-block (cadr program) (make-immutable-hash (map cons (map car program) program)) (make-immutable-hash (map cons (cdar program) data)))))
+    (run-block (cadr program) (create-label-map program) (create-env-map program data))))
 
 (define tm-int
   '((read Q Right)
@@ -109,13 +124,41 @@
     )
   )
 
+(define find-name
+  '((read name namelist valuelist)
+    (search (if (equal? name (car namelist)) found cont))
+    (cont (:= valuelist (cdr valuelist)) (:= namelist (cdr namelist)) (goto search))
+    (found (return (car valuelist)))
+    )
+  )
+
+(define fc-int
+  '((read program data)
+    (init (:= env (create-env-map program data)) (:= labels (create-label-map program))
+          (:= bb (cdadr program)) (goto loop))
+    (loop (if (empty? bb) loop-end loop-body))
+    (loop-body (:= command (car bb)) (:= bb (cdr-or-empty bb)) (goto case1))
+    (case1 (if (equal? (car command) ':=) do-assign case2))
+    (case2 (if (equal? (car command) 'goto) do-goto case3))
+    (case3 (if (equal? (car command) 'if) do-if case4))
+    (case4 (if (equal? (car command) 'return) do-return error))
+    (do-goto (:= bb (cdr (hash-ref labels (cadr command)))) (goto loop))
+    (do-if (if (eval-expr (cadr command) env) do-if-true do-if-false))
+    (do-if-true (:= bb (cdr (hash-ref labels (caddr command)))) (goto loop))
+    (do-if-false (:= bb (cdr (hash-ref labels (cadddr command)))) (goto loop))
+    (do-assign (:= env (update-env env (cadr command) (eval-expr (caddr command) env))) (goto loop))
+    (do-return (return (eval-expr (cadr command) env)))
+    (error (return (raise 'syntaxerror: command)))
+    )
+  )
+
 (define mix
   '((read program division vs0)
-    (init (:= pending (list (cons (caadr program) vs0))) (:= marked (list (cons (caar pending) (cdar pending))))
+    (init (:= pp0 (caadr program)) (:= pending (list (cons pp0 vs0))) (:= marked (list (cons (caar pending) (cdar pending))))
           (:= residual (list (cons 'read (get-dynamic-args program division)))) (goto loop))
     (loop (if (empty? pending) stop loop-body))
     (loop-body (:= pp (caar pending)) (:= vs (cdar pending)) (:= pending (cdr-or-empty pending)) 
-               (:= bb (lookup pp program)) (:= code (list (cons pp vs))) (goto inner-loop))
+               (:= code (list (cons pp vs))) (:= bb (lookup pp program)) (goto inner-loop))
     (inner-loop (if (empty? bb) loop-end inner-loop-body))
     (inner-loop-body (:= command (car bb)) (:= bb (cdr-or-empty bb)) (goto case1))
     (case1 (if (equal? (car command) ':=) do-assign case2))
@@ -123,7 +166,7 @@
     (case3 (if (equal? (car command) 'if) do-if case4))
     (case4 (if (equal? (car command) 'return) do-return error))
     (do-assign (if (is-static (cadr command) division) do-assign-static do-assign-dynamic))
-    (do-assign-static (:= vs (hash-set vs (cadr command) (eval-expr (caddr command) vs))) (goto inner-loop))
+    (do-assign-static (:= vs (update-env vs (cadr command) (eval-expr (caddr command) vs))) (goto inner-loop))
     (do-assign-dynamic (:= code (cons (list ':= (cadr command) (reduce (caddr command) vs)) code)) (goto inner-loop))
     (do-goto (:= bb (lookup (cadr command) program)) (goto inner-loop))
     (do-if (if (is-static (cadr command) division) do-if-static do-if-dynamic))
@@ -137,14 +180,27 @@
     (loop-end (:= residual (cons (reverse code) residual)) (goto loop))
     (stop (return (improve-labels (reverse residual))))
     (error (return (raise 'syntaxerror: command)))
+    (lookup (:= ppx pp0) (:= bb (lookup ppx program)) (goto lookup-loop))
+    (lookup-loop (if (equal? pp ppx) inner-loop lookup-body))
+    (lookup-body (:= ppx (next-label-after ppx program)) (:= bb (lookup ppx program)) (goto lookup-loop))
     )
   )
 
 (define tm-example '((0 if 0 goto 3) (1 right) (2 goto 0) (3 write 1)))
 
-(define fut1 (int mix `(,tm-int ,(list 'Q 'Qtail 'Instruction 'Operator 'Symbol 'NextLabel) ,(make-immutable-hash (list (cons 'Q tm-example))))))
+(define tm-int-division (list 'Q 'Qtail 'Instruction 'Operator 'Symbol 'NextLabel))
+;;(define fc-int-division (list 'program 'labels 'bb 'command))
+(define mix-division (list 'program 'division));; 'bb 'command 'pp0 'ppx))
 
-(pretty-print-columns 50)
+(define fut1 (int mix `(,tm-int ,tm-int-division ,(make-immutable-hash (list (cons 'Q (cons 'quote (list tm-example))))))))
+(define fut2 (int mix `(,mix ,mix-division ,(make-immutable-hash (list (cons 'program (cons 'quote (list tm-int))) (cons 'division (cons 'quote (list tm-int-division))))))))
+
+(pretty-print-columns 100)
+;;(pretty-print fut1)
+;;(pretty-print fut2)
+
+(define res1 (int fut2 `(,(make-immutable-hash (list (cons 'Q (cons 'quote (list tm-example))))))))
+(define res2 (int fut1 '((1 1 1 0 1 0 1))))
+(pretty-print res1)
 (pretty-print fut1)
-
-(int fut1 '((1 1 1 0 1 0 1)))
+(equal? res1 fut1)
